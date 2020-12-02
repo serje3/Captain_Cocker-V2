d888950d4d2cd9e2658038fdf0687fe07d933ae5
serje3 <serjerin322@gmail.com>
2020-12-02 23:43:42 +0300

    added

--- vkbot/app.js
+++ vkbot/app.js
@@ -1,5 +1,7 @@
 const {settings} = require('./settings')
-const { VK } = require('vk-io')
+const { VK, Keyboard } = require('vk-io')
+const { HearManager } = require('@vk-io/hear')
+let chats = require('./chats.json')
 const botDiscord = require('./botDiscordData')
 const toDiscord = new botDiscord() 
 const vk = new VK({
@@ -7,6 +9,53 @@ const vk = new VK({
     pollingGroupId: settings.GROUP_ID,
     apiMode: settings.API_MODE
 })
+
+// setInterval(async () => {
+// 	console.log(chats)
+//     await fs.writeFileSync('./chats.json', JSON.stringify(chats,null,'\t'))
+// }, 1000)
+
+const hearManager = new HearManager();
+
+vk.updates.on('message_new', (context, next) => {
+	const { messagePayload } = context;
+
+	context.state.command = messagePayload && messagePayload.command
+		? messagePayload.command
+		: null;
+
+	return next();
+});
+
+vk.updates.on('message_new', hearManager.middleware);
+
+// Simple wrapper for commands
+const hearCommand = (name, conditions, handle) => {
+	if (typeof handle !== 'function') {
+		handle = conditions;
+		conditions = [`/${name}`];
+	}
+
+	if (!Array.isArray(conditions)) {
+		conditions = [conditions];
+	}
+
+	hearManager.hear(
+		[
+			(text, { state }) => (
+				state.command === name
+			),
+			...conditions
+		],
+		handle
+	);
+};
+
+async function saveChats(){
+	require('fs').writeFileSync(`./chats.json`, JSON.stringify(chats,null,'\t'))
+	return true;
+}
+
 
 vk.updates.use(async (msg,next)=>{
     if(msg.senderType !== 'user') return false;
@@ -16,14 +65,25 @@ vk.updates.use(async (msg,next)=>{
     }
 
     if (msg.text !== undefined) {
-        console.log(msg);
-        console.log(msg.senderId)
+        // console.log(msg);
+        // console.log(msg.senderId)
+		const chat = chats.filter(item => item.chatId === msg.chatId)[0]
+		console.log('работает?',chats)
+    	if (!chat){
 
-        if (msg.text.trim()==='!nigger'){
+        chats.push({
+            chatId:msg.chatId,
+            channel: -1
+        })
+			console.log('ну и', chats)
+			saveChats()
+    	}
+
+        if (msg.text.trim().startsWith('!nigger')){
             vk.api.messages.removeChatUser({
                 chat_id: msg.chatId,
                 user_id: msg.senderId,
-            });
+            }).catch(e => msg.send("Сучька"));
         }
         else {
             msg.text.trim()[0] === '!' ? toDiscord.handleMessage(msg.text.trim()).then((value) => {
@@ -37,6 +97,73 @@ vk.updates.use(async (msg,next)=>{
 
 
 
+hearCommand('start', (context, next)=>{
+    context.state.command = 'help';
+
+	return Promise.all([
+		context.send('Але'),
+
+		next()
+	]);
+})
+
+
+hearCommand('help', async (context) => {
+	await context.send({
+		message: `
+			Список команд
+			/setChannel - установить главный канал
+			!join - присоединиться к голосовому чату в дискорде
+			!playlist - Включить плейлист в голосовом чате в дискорде
+			!showList - Показать список песен
+			!nigger - стать счастливым, избравиться от депрессии, получить много денег, нобелевскую премию, жену, сына, дочь, 10 килограммого сома из реки Ахтуба, преисполниться в этой жизни, постичь дзен, прекратить все войны, болезни, нищеты, воровство, несправедливость
+		`,
+		keyboard: Keyboard.builder()
+			.textButton({
+				label: 'Присоединиться к голосовому чату',
+				payload: {
+					command: 'join'
+				},
+                color: Keyboard.POSITIVE_COLOR
+			})
+			.row()
+			.textButton({
+				label: 'Включить плейлист',
+				payload: {
+					command: 'playlist'
+				},
+				color: Keyboard.PRIMARY_COLOR
+			})
+			.textButton({
+				label: 'Остановить и выйти',
+				payload: {
+					command: 'stop'
+				},
+				color: Keyboard.PRIMARY_COLOR
+			}).row()
+            .textButton({
+				label: 'nigger',
+				payload: {
+					command: 'nigger'
+				},
+				color: Keyboard.NEGATIVE_COLOR
+			}),
+	});
+});
+
+
+hearCommand('join', async (msg)=>{
+    toDiscord.handleMessage('!join bruh').then((value) => {
+                msg.send(value)
+            }).catch(e => console.log(e))
+})
+
+hearCommand('nigger', async (context) =>{
+    vk.api.messages.removeChatUser({
+                chat_id: context.chatId,
+                user_id: context.senderId,
+            }).catch(e => context.send("Сучька"));
+})
 
 
 
--- vkbot/settings.js
+++ vkbot/settings.js
@@ -1,6 +1,6 @@
 module.exports.settings = {
     //to auth vk api
-    TOKEN: process.env.TOKEN_VK,
+    TOKEN: '125320e88150e67d4449b56949406261c8aa8f64ea4b68fce565b9ea0ca47f0b89ef1f5feb08e6639bb04',
     GROUP_ID:process.env.GROUPID,
     API_MODE: "parallel",
 
