0340e590912bef513a97cbaaec0b8e87636180d7
serje3 <serjerin322@gmail.com>
2020-12-02 17:06:47 +0300

    Updated to heroku

--- settings.py
+++ settings.py
@@ -3,5 +3,5 @@ import os
 TOKEN = os.environ.get('TOKEN') #discord bot token
 
 AUTHKEY = os.environ.get('TOKEN')#", " " etc., should equal AUTHKEY in settings.js"
-URL = '0.0.0.0'
-PORT = 5000 #int
+URL = os.environ.get('URL')
+PORT = os.environ.get('PORT') #int
--- vkbot/settings.js
+++ vkbot/settings.js
@@ -1,11 +1,11 @@
 module.exports.settings = {
     //to auth vk api
-    TOKEN: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-    GROUP_ID:321312231,
+    TOKEN: process.env.TOKEN_VK,
+    GROUP_ID:process.env.GROUPID,
     API_MODE: "parallel",
 
     //fetch()
-    URL:'0.0.0.0:5000 should equal AUTHKEY in settings.py', //URL + PORT + '/'
-    AUTHKEY:'any character set, but not "#", " " etc., should equal AUTHKEY in settings.py'
+    URL:'https://captain-cocker.herokuapp.com/', //URL + PORT + '/'
+    AUTHKEY: process.env.AUTHKEY
 
 }
